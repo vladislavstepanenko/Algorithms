f0a930fb7104ab1faf53c57460d1ff57a5dbe60d
Vladislav <vlad.stepanenko996@gmail.com>
2021-06-11 01:31:51 +1000

    Selection Sort

---
+++ selection_sort/selection_sort.js
@@ -0,0 +1,26 @@
+
+const findSmallest = (arr) => {
+	let smallest = arr[0];
+	let smallest_index = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < smallest) {
+			smallest = arr[i];
+			smallest_index = i;
+		}
+	}
+	return smallest_index;
+};
+
+const selectionSort = (arr) => {
+	newArr = [];
+	let L = arr.length;
+	for (let j = 0; j < L; j++) {
+		smallest = findSmallest(arr);
+		newArr.push(arr[smallest]);
+		arr.splice(smallest, 1);
+	}
+	return newArr;
+}
+let a = [5, 3, 4, 2, 10, 190, -10, -39];
+console.log(selectionSort(a));
